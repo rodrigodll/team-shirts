13447548da4b89fd6f40463a86b96897248a2e4f
Rodrigo Nascimento <rodrigo.nts@globo.com>
2020-05-17 23:40:53 -0300

    Updates

--- precache-manifest.a38c46591e8f1f3db8cf6f433d87db33.js
+++ precache-manifest.a38c46591e8f1f3db8cf6f433d87db33.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "393bd74d21600050b8ec0b938f113f31",
+    "revision": "b6d56ce9bcc0d9c0e0b05fdb7c6317c6",
     "url": "/team-shirts/index.html"
   },
   {
-    "revision": "0f33c1eee87daf95ebbe",
-    "url": "/team-shirts/static/css/main.fff8ae6d.chunk.css"
+    "revision": "90a13b9f588b4e80b01b",
+    "url": "/team-shirts/static/css/main.4b8dbc81.chunk.css"
   },
   {
     "revision": "6f118abecbcab66ada22",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/team-shirts/static/js/2.7cc5d21a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0f33c1eee87daf95ebbe",
+    "revision": "90a13b9f588b4e80b01b",
     "url": "/team-shirts/static/js/main.797786b8.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/team-shirts/precache-manifest.557dacbccafccd560be182439a131bef.js"
+  "/team-shirts/precache-manifest.a38c46591e8f1f3db8cf6f433d87db33.js"
 );
 
 self.addEventListener('message', (event) => {
