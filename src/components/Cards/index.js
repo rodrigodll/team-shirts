d15eb904f116bca604ecc337012a7754ab5cf8ed
Rodrigo Nascimento <rodrigo.nts@globo.com>
2020-05-17 22:38:56 -0300

    melhorias de interação

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -22,18 +22,20 @@ export default class Cards extends Component {
       const renderCard = this.state.data.map(item => {
         return(
           <div className="card">
-            <h2>{item.shortName}</h2>
-            <i className="fas fa-arrow-right"></i>
-            <p>{item.material[0].name}</p>
-            <img src={item.material[0].logo} alt={item.material[0].name} />
-            <div className="pic" style={{backgroundImage: "url("+item.image+")"}}></div>
-            <div className="social">
-                {item.social[0].facebook ? <a href={item.social[0].facebook} target="_blank"><i className="fab fa-facebook-f"></i></a> : ''}
-                {item.social[0].instagram ? <a href={item.social[0].instagram} target="_blank"><i className="fab fa-instagram"></i></a> : ''}
-                {item.social[0].twitter ? <a href={item.social[0].twitter} target="_blank"><i className="fab fa-twitter"></i></a> : ''}
-                {item.social[0].youtube ? <a href={item.social[0].youtube} target="_blank"><i className="fab fa-youtube"></i></a> : ''}
+            <h2 className="card__name">{item.shortName}</h2>
+            <button className="card__trigger" onClick={() => alert('Mostrar detalhe da camisa')}>
+              <i className="fas fa-arrow-right"></i>
+            </button>
+            <p className="card__material">{item.material[0].name}</p>
+            <img className="card__material-logo" src={item.material[0].logo} alt={item.material[0].name} />
+            <div className="card__pic" style={{backgroundImage: "url("+item.image+")"}}></div>
+            <div className="card__social">
+                {item.social[0].facebook ? <a href={item.social[0].facebook} target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a> : ''}
+                {item.social[0].instagram ? <a href={item.social[0].instagram} target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a> : ''}
+                {item.social[0].twitter ? <a href={item.social[0].twitter} target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a> : ''}
+                {item.social[0].youtube ? <a href={item.social[0].youtube} target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube"></i></a> : ''}
             </div>
-            <button style={{backgroundColor: item.color}} onClick={() => alert('Mostrar detalhe da camisa')}>
+            <button className="card__button" style={{backgroundColor: item.color}}>
             </button>
           </div>
         )
