424a325d0a8a6c215145ace7dc62eb42eb1facc4
Rodrigo Nascimento <rodrigo.nts@globo.com>
2020-05-17 21:39:31 -0300

    Updates

---
+++ precache-manifest.55e7f4c9b1255310839d5dee8b72b0a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef42238456118a920afd1a32b71f3920",
+    "url": "/team-shirts/index.html"
+  },
+  {
+    "revision": "d4a53996cf475d8e13ab",
+    "url": "/team-shirts/static/css/main.4729713a.chunk.css"
+  },
+  {
+    "revision": "6f118abecbcab66ada22",
+    "url": "/team-shirts/static/js/2.7cc5d21a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/team-shirts/static/js/2.7cc5d21a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a53996cf475d8e13ab",
+    "url": "/team-shirts/static/js/main.00f50062.chunk.js"
+  },
+  {
+    "revision": "3547da2bf1f8a173343e",
+    "url": "/team-shirts/static/js/runtime-main.d94da8fe.js"
+  }
+]);
