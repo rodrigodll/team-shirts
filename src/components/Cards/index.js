e154cd43737cee52c8396aba63e0e5a490826231
Rodrigo Nascimento <rodrigo.nts@globo.com>
2020-05-20 01:49:43 -0300

    componente de busca

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,64 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 import './Cards.scss';
 
-class Cards extends Component {
+// propriedas passadas ao component
+// data: arquivo base de informações
+// filterText: Parâmetro criado em App.js para uso em Search.js
+export default ({ data, filterText }) => {
 
-    render() {
-      const { data } = this.props
-
-      const renderCard = data.map(item => {
-        return(
-          <div className="card" key={item.id}>
-            <h2 className="card__name">{item.shortName}</h2>
-            {/* <button className="card__trigger" onClick={() => {this.setState({detail: item}); this.viewDetails()}}> */}
-            <button className="card__trigger">
-              <i className="fas fa-arrow-right"></i>
-            </button>
-            <p className="card__material">{item.material[0].name}</p>
-            <img className="card__material-logo" src={item.material[0].logo} alt={item.material[0].name} />
-            <div className="card__pic" style={{backgroundImage: "url("+item.image+")"}}></div>
-            <div className="card__social">
-                {item.social[0].facebook ?
-                  <a href={item.social[0].facebook} target="_blank" rel="noopener noreferrer" style={{backgroundColor: item.color}}>
-                    <i className="fab fa-facebook-f">
-                    </i></a>: ''}
-                {item.social[0].instagram ?
-                  <a href={item.social[0].instagram} target="_blank" rel="noopener noreferrer" style={{backgroundColor: item.color}}>
-                    <i className="fab fa-instagram"></i>
-                  </a>: ''}
-                {item.social[0].twitter ?
-                  <a href={item.social[0].twitter} target="_blank" rel="noopener noreferrer" style={{backgroundColor: item.color}}>
-                    <i className="fab fa-twitter"></i>
-                  </a>: ''}
-                {item.social[0].youtube ?
-                  <a href={item.social[0].youtube} target="_blank" rel="noopener noreferrer" style={{backgroundColor: item.color}}>
-                    <i className="fab fa-youtube"></i>
-                  </a>: ''}
-            </div>
-            <button
-              className="card__button" 
-              style={
-                (item.color === "#000") ? {
-                  opacity: "0.8",
-                  backgroundColor: item.color
-                } : { backgroundColor: item.color }
-                }
-              >
-            </button>
-          </div>
-        )
-      })
-  
-      return (
-        <div className="cards">
-          <div className="container">
-            <div className="row">
-              {renderCard}
-            </div>
-          </div>
+  const renderCard = data
+  .filter(item => {
+    // filtro na página com base no valor da busca
+    // item.ShortName: Fluminense | filterText: Fluminense 
+    return item.shortName.toLowerCase().indexOf(filterText.toLowerCase()) >= 0
+  })
+  .map(item => {
+    // Renderiza os cards na tela
+    return(
+      <div className="card" key={item.id}>
+        <h2 className="card__name">{item.shortName}</h2>
+        {/* <button className="card__trigger" onClick={() => {this.setState({detail: item}); this.viewDetails()}}> */}
+        <button className="card__trigger">
+          <i className="fas fa-arrow-right"></i>
+        </button>
+        <p className="card__material">{item.material[0].name}</p>
+        <img className="card__material-logo" src={item.material[0].logo} alt={item.material[0].name} />
+        <div className="card__pic" style={{backgroundImage: "url("+item.image+")"}}></div>
+        <div className="card__social">
+            {item.social[0].facebook ?
+              <a href={item.social[0].facebook} target="_blank" rel="noopener noreferrer" style={{backgroundColor: item.color}}>
+                <i className="fab fa-facebook-f">
+                </i></a>: ''}
+            {item.social[0].instagram ?
+              <a href={item.social[0].instagram} target="_blank" rel="noopener noreferrer" style={{backgroundColor: item.color}}>
+                <i className="fab fa-instagram"></i>
+              </a>: ''}
+            {item.social[0].twitter ?
+              <a href={item.social[0].twitter} target="_blank" rel="noopener noreferrer" style={{backgroundColor: item.color}}>
+                <i className="fab fa-twitter"></i>
+              </a>: ''}
+            {item.social[0].youtube ?
+              <a href={item.social[0].youtube} target="_blank" rel="noopener noreferrer" style={{backgroundColor: item.color}}>
+                <i className="fab fa-youtube"></i>
+              </a>: ''}
         </div>
-      )
-    }
-}
+        <button
+          className="card__button" 
+          style={
+            (item.color === "#000") ? {
+              opacity: "0.8",
+              backgroundColor: item.color
+            } : { backgroundColor: item.color }
+            }
+          >
+        </button>
+      </div>
+    )
+  })
 
-export default Cards;
+  return (
+    <div className="cards">
+      <div className="container">
+        <div className="row">
+          {renderCard}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/Data/data.js
+++ src/components/Data/data.js
@@ -1,15 +1,15 @@
 export default [
     {
-        id: 1,
-        shortName: 'Fluminense',
-        abv: 'Flu',
+        id: 5,
+        shortName: 'Bangu',
+        abv: 'BAN',
         material: [{
-            name: 'Umbro',
-            logo: 'https://i.pinimg.com/originals/5f/04/0e/5f040e809b7ab60c71f5ea24d0b5687c.png',
-            site: 'https://www.umbro.com.br/fluminense'
+            name: 'W A Sports',
+            logo: 'https://www.wasport.com.br/simulador/simulador/img/logos/wasport.png',
+            site: '/'
         }],
-        site: 'https://www.fluminense.com.br',
-        color: 'rgb(86, 19, 19)',
+        site: 'https://www.bangu-ac.com.br/',
+        color: 'red',
         logo: 'logo',
         shirt: [{
             home: 'Casa',
@@ -17,12 +17,35 @@ export default [
             third: 'Terceira camisa'
         }],
         social: [{
-            facebook: 'https://www.facebook.com/FluminenseFC/',
-            instagram: 'https://www.instagram.com/fluminensefc/',
-            youtube: 'https://www.youtube.com/fluminensefc',
-            twitter: 'https://twitter.com/fluminensefc'
+            facebook: 'https://www.facebook.com/BanguOficial/',
+            twitter: 'https://twitter.com/Banguoficial',
+            youtube: 'https://www.youtube.com/channel/UCVy9x9KVuKFDnOnkv_bKRMg'
         }],
-        image: 'https://www.tupi.fm/wp-content/uploads/camisa-flu-1.jpg'
+        image: 'https://www.bangu-ac.com.br/wp-content/uploads/WhatsApp-Image-2019-11-21-at-14.51.30.jpeg'
+    },
+    {
+        id: 4,
+        shortName: 'Botafogo',
+        abv: 'Bot',
+        material: [{
+            name: 'Kappa',
+            logo: 'https://www.botafogo.com.br/images/partners/kappa_logo2019.png',
+            site: '/'
+        }],
+        site: 'https://www.botafogo.com.br/',
+        color: '#000',
+        logo: 'logo',
+        shirt: [{
+            home: 'Casa',
+            homeaway: 'Fora',
+            third: 'Terceira camisa'
+        }],
+        social: [{
+            facebook: 'http://www.facebook.com/botafogo',
+            instagram: 'http://instagram.com/botafogo',
+            twitter: 'http://twitter.com/botafogo'
+        }],
+        image: 'https://conteudo.imguol.com.br/c/esporte/07/2020/01/31/botafogo-anuncia-contratacao-do-japones-keisuke-honda-1580512481155_v2_1080x1055.jpg'
     },
     {
         id: 2,
@@ -49,6 +72,31 @@ export default [
         image: 'https://colunadofla.com/wp-content/uploads/2016/08/flamengo-uniforme.png'
     },
     {
+        id: 1,
+        shortName: 'Fluminense',
+        abv: 'Flu',
+        material: [{
+            name: 'Umbro',
+            logo: 'https://i.pinimg.com/originals/5f/04/0e/5f040e809b7ab60c71f5ea24d0b5687c.png',
+            site: 'https://www.umbro.com.br/fluminense'
+        }],
+        site: 'https://www.fluminense.com.br',
+        color: 'rgb(86, 19, 19)',
+        logo: 'logo',
+        shirt: [{
+            home: 'Casa',
+            homeaway: 'Fora',
+            third: 'Terceira camisa'
+        }],
+        social: [{
+            facebook: 'https://www.facebook.com/FluminenseFC/',
+            instagram: 'https://www.instagram.com/fluminensefc/',
+            youtube: 'https://www.youtube.com/fluminensefc',
+            twitter: 'https://twitter.com/fluminensefc'
+        }],
+        image: 'https://www.tupi.fm/wp-content/uploads/camisa-flu-1.jpg'
+    },
+    {
         id: 3,
         shortName: 'Vasco da Gama',
         abv: 'Vas',
@@ -71,29 +119,5 @@ export default [
             youtube: 'youtbe'
         }],
         image: 'https://www.lance.com.br/files/article_main/uploads/2019/08/20/5d5c53a72fe3f.jpeg'
-    },
-    {
-        id: 4,
-        shortName: 'Botafogo',
-        abv: 'Bot',
-        material: [{
-            name: 'Kappa',
-            logo: 'https://www.botafogo.com.br/images/partners/kappa_logo2019.png',
-            site: '/'
-        }],
-        site: 'https://www.botafogo.com.br/',
-        color: '#000',
-        logo: 'logo',
-        shirt: [{
-            home: 'Casa',
-            homeaway: 'Fora',
-            third: 'Terceira camisa'
-        }],
-        social: [{
-            facebook: 'http://www.facebook.com/botafogo',
-            instagram: 'http://instagram.com/botafogo',
-            twitter: 'http://twitter.com/botafogo'
-        }],
-        image: 'https://conteudo.imguol.com.br/c/esporte/07/2020/01/31/botafogo-anuncia-contratacao-do-japones-keisuke-honda-1580512481155_v2_1080x1055.jpg'
     }
 ]
